c997b4d216f805801942615aef768327ae745e5d
alice <alicward@gmail.com>
2018-01-15 15:11:10 +0100

    dodanie sekci co tu znajdziesz

--- js/home.js
+++ js/home.js
@@ -2,7 +2,7 @@ function inizializeHomeAnimation() {
   setTimeout(function() {
     var flower = new Vivus(
       "flower",
-      { duration: 200, file: "./images/flower.svg" },
+      { type: "oneByOne", duration: 150, file: "./images/flower.svg" },
       function() {}
     );
   }, 20);
